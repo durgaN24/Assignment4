a7913853a1f506684d49cda1e54d4978e6b6cd52
durgaN24 <96798708+durgaN24@users.noreply.github.com>
2022-01-03 23:36:31 -0600

    classes and objects
    Created classes and objects

--- A1.py
+++ A1.py
@@ -1,23 +1,31 @@
 
 
 import csv
-with open('numberfile.csv', 'r') as a:
-    r = csv.reader(a)
-    for row in r:
-        print(row)
+numfile = "numberfile.csv"
+class assignment4:
 
+    def readwrite(f1):
+        with open(f1 , "r") as a:
+            r = a.readlines ()
+            for row in r:
+                print(row)
+    
+    def sum1(f):
+        with open(f, "r") as file1:
+            lines = file1.readlines ()
 
-given_file = open('numberfile.csv', 'r')
+            sum = 0
 
-lines = given_file.readlines ()
+            for line in lines:
+             for c in line:
+                if c.isdigit() == True:
+                    sum = sum + int (c)
+            
+            print (sum)
+            file1.close()
+            return sum
+            
 
-sum = 0
+    readwrite(numfile);
+    sum1(numfile)
 
-for line in lines:
-    for c in line:
-        if c.isdigit() == True:
-            sum = sum + int (c)
-
-print (sum)
-
-given_file.close()
